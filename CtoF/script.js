de2370dfecdeb21495cd88f898bd529628d472e1
Md. Mozammal Haq <mrmozammal@gmail.com>
2024-09-09 21:14:08 +0600

    Add another One

--- CtoF/script.js
+++ CtoF/script.js
@@ -1,20 +1,20 @@
 // DOM Elements
-const fahrenheitInput = document.getElementById("fahrenheit");
+const celsiusInput = document.getElementById("celsius");
 const convertBtn = document.getElementById("convertBtn");
 const resultParagraph = document.getElementById("result");
 
 // Function to convert Celsius to Fahrenheit
 function convertTemperature() {
-  const fahrenheit = fahrenheitInput.value;
-  if (fahrenheit === "") {
+  const celsius = celsiusInput.value;
+  if (celsius === "") {
     resultParagraph.textContent = "Please enter a value.";
-    fahrenheitInput.value = "";
+    celsiusInput.value = "";
   } else {
-    const celsius = ((fahrenheit - 32) * 5) / 9;
-    resultParagraph.textContent = `${fahrenheit}°C is equal to ${celsius.toFixed(
+    const fahrenheit = (celsius * 9) / 5 + 32;
+    resultParagraph.textContent = `${celsius}°C is equal to ${fahrenheit.toFixed(
       2
     )}°F`;
-    fahrenheitInput.value = "";
+    celsiusInput.value = "";
   }
 }
 
